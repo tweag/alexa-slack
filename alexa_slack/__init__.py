99d4bd01ae8e0138cfe932cb327ce079eeb55f6d
Patrick Smith <patrick@promptworks.com>
2016-05-06 13:39:13 -0400

    Refactor application

---
+++ alexa_slack/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+from pylexa.app import alexa_blueprint
+from pylexa.response import AlexaResponseWrapper
+
+
+app = Flask(__name__)
+app.register_blueprint(alexa_blueprint)
+app.response_class = AlexaResponseWrapper
+
+
+@app.before_request
+def log_request_json():
+    print request.headers
+    print request.json
+
+
+import alexa_slack.intent_handlers
+import alexa_slack.oauth
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8444)
--- alexa_slack/intent_handlers.py
+++ alexa_slack/intent_handlers.py
@@ -1,32 +1,6 @@
-# -*- coding: utf-8 -*-
-from collections import OrderedDict
-from urllib import urlencode
-import os
-
-from flask import Flask, redirect, request
-import requests
-
-from pylexa.app import alexa_blueprint, handle_launch_request
+from pylexa.app import handle_launch_request
 from pylexa.intent import handle_intent
-from pylexa.response import (
-    AlexaResponseWrapper, LinkAccountCard, PlainTextSpeech, Response
-)
-
-
-app = Flask(__name__)
-app.register_blueprint(alexa_blueprint)
-app.response_class = AlexaResponseWrapper
-
-AWS_VENDOR_ID = os.getenv('AWS_VENDOR_ID')
-SLACK_CLIENT_ID = os.getenv('SLACK_CLIENT_ID')
-SLACK_CLIENT_SECRET = os.getenv('SLACK_CLIENT_SECRET')
-SLACK_CHANNEL = os.getenv('SLACK_CHANNEL', 'botstuff')
-
-
-@app.before_request
-def log_request_json():
-    print request.headers
-    print request.json
+from pylexa.response import LinkAccountCard, PlainTextSpeech, Response
 
 
 @handle_intent('SetChannel')
@@ -92,47 +66,12 @@ def handle_start_message(request):
             speech=PlainTextSpeech('You must sign in first'),
             card=LinkAccountCard(),
         )
-    speech = PlainTextSpeech('What channel would you like to post to?')
+    speech = PlainTextSpeech('What channel would you like to post your message to?')
     reprompt = PlainTextSpeech('Say the name of the channel you would like to post to')
     return Response(speech=speech, reprompt=reprompt, should_end_session=False)
 
 
 @handle_intent('unrecognized_intent')
 def handle_unrecognized_intent(request):
-    return PlainTextSpeech("I'm sorry I didn't understand that")
+    return PlainTextSpeech("I'm sorry, I didn't understand that")
 
-
-@app.route('/oauth/entry')
-def oauth_entry_point():
-    client_id = request.args.get('client_id')
-    scope = request.args.get('scope')
-    state = request.args.get('state')
-    url = 'https://slack.com/oauth/authorize'
-    query_string = urlencode(OrderedDict(
-        scope=scope,
-        state=state,
-        client_id=client_id
-    ))
-    redirect_url = '{}?{}'.format(url, query_string)
-    return redirect(redirect_url)
-
-
-@app.route('/oauth/redirect')
-def oauth_redirect():
-    state = request.args.get('state')
-    code = request.args.get('code')
-
-    response = requests.get(
-        'https://slack.com/api/oauth.access?{}'.format(urlencode({ 'code': code })),
-        auth=(SLACK_CLIENT_ID, SLACK_CLIENT_SECRET)
-    )
-    response_json = response.json()
-    access_token = response_json.get('access_token')
-
-    redirect_url = 'https://pitangui.amazon.com/spa/skill/account-linking-status.html?vendorId={}'.format(AWS_VENDOR_ID)
-    redirect_url += '#{}'.format(urlencode({'access_token': access_token, 'state': state, 'token_type': 'Bearer'}))
-    return redirect(redirect_url)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8444)
